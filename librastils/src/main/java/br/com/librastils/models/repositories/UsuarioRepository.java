2e65aea2bc056c4c5962007238deea19e54fc46b
Israellopes <143666711+israellopes19@users.noreply.github.com>
2024-10-07 19:58:44 -0300

    Delete librastils/src/main/java/br/com/librastils/models/repositories directory

--- librastils/src/main/java/br/com/librastils/models/repositories/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.librastils.repositories;
-
-import br.com.librastils.models.usuario.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, String > {
-
-}
